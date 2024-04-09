0be3c9dc4f849b2e6995559c8a96479c387b156e
ashley-koh <ashleykoh24@gmail.com>
2024-04-09 18:07:22 +0800

    update: added docs for random promo

--- src/main/java/com/ashleykoh/promojioserver/repositories/CustomPromoRepositoryImpl.java
+++ src/main/java/com/ashleykoh/promojioserver/repositories/CustomPromoRepositoryImpl.java
@@ -13,7 +13,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Component
